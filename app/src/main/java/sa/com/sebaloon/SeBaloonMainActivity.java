1746156c9889103dba9990ea25daf14f312b3fef
ramdesh <rasade88@gmail.com>
2016-05-18 16:27:03 +0530

    Added value reporting from sensors

--- app/src/main/java/sa/com/sebaloon/SeBaloonMainActivity.java
+++ app/src/main/java/sa/com/sebaloon/SeBaloonMainActivity.java
@@ -7,38 +7,86 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SeBaloonMainActivity extends AppCompatActivity {
 
     private SensorManager manager;
     private SensorEventListener listener;
+    private TextView accelerometerValue;
+    private TextView gyroscopeValue;
+    private TextView magneticFieldValue;
+    private TextView barometerValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_se_baloon_main);
+        accelerometerValue = (TextView)findViewById(R.id.accelerometerValue);
+        gyroscopeValue = (TextView)findViewById(R.id.gyroscopeValue);
+        magneticFieldValue = (TextView)findViewById(R.id.magneticFieldValue);
+        barometerValue = (TextView)findViewById(R.id.barometerValue);
 
         manager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
 
-        listener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                Sensor mSensor = event.sensor;
-                if(mSensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                    // Accelerometer stuff
-                } else if (mSensor.getType() == Sensor.TYPE_GYROSCOPE) {
-                    // Gyroscope stuff
-                } else if (mSensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-                    // Magnetometer stuff
-                } else if (mSensor.getType() == Sensor.TYPE_PRESSURE) {
-                    // Barometer stuff
+        Thread t = new Thread() {
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            listener = new SensorEventListener() {
+                                @Override
+                                public void onSensorChanged(SensorEvent event) {
+                                    Sensor mSensor = event.sensor;
+                                    if (mSensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                                        float acceloX = event.values[0];
+                                        float acceloY = event.values[0];
+                                        String displayString = "X: " + acceloX + " | Y: " + acceloY;
+                                        accelerometerValue.setText(displayString);
+                                    } else if (mSensor.getType() == Sensor.TYPE_GYROSCOPE) {
+                                        float gyroX = event.values[0];
+                                        float gyroY = event.values[1];
+                                        float gyroZ = event.values[2];
+                                        String displayString = "X: " + gyroX + " | Y: " + gyroY + " | Z: " + gyroZ;
+                                        gyroscopeValue.setText(displayString);
+                                    } else if (mSensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                                        float magneticX = event.values[0];
+                                        float magneticY = event.values[1];
+                                        float magneticZ = event.values[2];
+                                        String displayString = "X: " + magneticX + " | Y: " + magneticY + " | Z: " + magneticZ;
+                                        magneticFieldValue.setText(displayString);
+                                    } else if (mSensor.getType() == Sensor.TYPE_PRESSURE) {
+                                        float pressure = event.values[0];
+                                        String displayString = pressure + "hPa";
+                                        barometerValue.setText(displayString);
+                                    }
+                                }
+
+                                @Override
+                                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+                                }
+                            };
+                            manager.registerListener(listener, manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+                            manager.registerListener(listener, manager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
+                            manager.registerListener(listener, manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_NORMAL);
+                            manager.registerListener(listener, manager.getDefaultSensor(Sensor.TYPE_PRESSURE), SensorManager.SENSOR_DELAY_NORMAL);
+                        }
+                    });
+
+                } catch(InterruptedException ie) {
+                    ie.printStackTrace();
                 }
             }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
         };
+
+        t.start();
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
