c47e284c5e7ddcc72668927ac30656a460c7488a
Ramindu Deshapriya <rasade88@gmail.com>
2016-05-19 01:26:04 +0530

    Added location capabilities

--- app/src/main/java/sa/com/sebaloon/SeBaloonMainActivity.java
+++ app/src/main/java/sa/com/sebaloon/SeBaloonMainActivity.java
@@ -1,22 +1,43 @@
 package sa.com.sebaloon;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class SeBaloonMainActivity extends AppCompatActivity {
 
-    private SensorManager manager;
-    private SensorEventListener listener;
+    private SensorManager sensorManager;
+    private LocationManager locationManager;
+
+    private SensorEventListener sensorEventListener;
+    private LocationListener locationListener;
+
     private TextView accelerometerValue;
     private TextView gyroscopeValue;
     private TextView magneticFieldValue;
     private TextView barometerValue;
+    private TextView gpsValue;
+
+    private static final String TAG = "SeBaloon";
+
+    private Location mLastLocation;
+
+    // Location updates intervals in sec
+    private static int UPDATE_INTERVAL = 10000; // 10 sec
+    private static int FATEST_INTERVAL = 5000; // 5 sec
+    private static int DISPLACEMENT = 10; // 10 meters
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +47,17 @@ public class SeBaloonMainActivity extends AppCompatActivity {
         gyroscopeValue = (TextView)findViewById(R.id.gyroscopeValue);
         magneticFieldValue = (TextView)findViewById(R.id.magneticFieldValue);
         barometerValue = (TextView)findViewById(R.id.barometerValue);
+        gpsValue = (TextView)findViewById(R.id.gpsValue);
 
-        manager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        sensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        // Acquire a reference to the system Location Manager
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
 
         Thread t = new Thread() {
             public void run() {
@@ -36,7 +66,7 @@ public class SeBaloonMainActivity extends AppCompatActivity {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            listener = new SensorEventListener() {
+                            sensorEventListener = new SensorEventListener() {
                                 @Override
                                 public void onSensorChanged(SensorEvent event) {
                                     Sensor mSensor = event.sensor;
@@ -69,12 +99,42 @@ public class SeBaloonMainActivity extends AppCompatActivity {
 
                                 }
                             };
-                            manager.registerListener(listener, manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-                            manager.registerListener(listener, manager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
-                            manager.registerListener(listener, manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_NORMAL);
-                            manager.registerListener(listener, manager.getDefaultSensor(Sensor.TYPE_PRESSURE), SensorManager.SENSOR_DELAY_NORMAL);
+                            sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+                            sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
+                            sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_NORMAL);
+                            sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE), SensorManager.SENSOR_DELAY_NORMAL);
+                            locationListener = new LocationListener() {
+                                @Override
+                                public void onLocationChanged(Location location) {
+                                    String displayString = "Lat: " + location.getLatitude() + " | Long: " + location.getLongitude();
+                                    gpsValue.setText(displayString);
+                                }
+
+                                @Override
+                                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                                }
+
+                                @Override
+                                public void onProviderEnabled(String provider) {
+
+                                }
+
+                                @Override
+                                public void onProviderDisabled(String provider) {
+
+                                }
+                            };
+                            try {
+                                // Register the sensorEventListener with the Location Manager to receive location updates
+                                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                            } catch(SecurityException se) {
+                                se.printStackTrace();
+                            }
+
                         }
                     });
+
 
                 } catch(InterruptedException ie) {
                     ie.printStackTrace();
@@ -88,5 +148,16 @@ public class SeBaloonMainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+
     }
 }
